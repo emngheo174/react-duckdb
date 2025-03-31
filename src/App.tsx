14490c0b0ddafb80b78e4df6cf62be62d26cbe6b
Kienmike <kienbr9@gmail.com>
2025-04-01 01:16:48 +0700

    update input sql query

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App h-screen flex flex-col justify-center">
-      <h1 className="text-2xl">House Price Analytics</h1>
+      <h1 className="text-2xl">Data Analytics</h1>
       <DataTable />
     </div>
   );
--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -27,12 +27,47 @@ async function instantiate(duckdb: any) {
 }
 
 const App: React.FC = () => {
+  const dbRef = useRef<any>(null);
+  const connRef = useRef<any>(null);
   const [dataLoaded, setDataLoaded] = useState(false);
+  const [createLoading, setCreateLoading] = useState(false);
   const [statsData, setStatsData] = useState<any[]>([]);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [originalQuery, setoriginalQuery] = useState<string>(`
+  const [databases, setDatabases] = useState<string[]>([]);
+  const [selectedDatabase, setSelectedDatabase] = useState<string | null>(null);
+  const [originalQuery, setoriginalQuery] = useState<string>(``);
+  const [customQuery, setCustomQuery] = useState<string>("");
+
+  useEffect(() => {
+    const initDB = async () => {
+      if (!dbRef.current) {
+        dbRef.current = await instantiate(duckdb); // Chỉ khởi tạo DuckDB một lần
+        connRef.current = await dbRef.current.connect();
+      }
+    };
+    initDB();
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedDatabase]);
+
+  const fetchData = async () => {
+    if (dataLoaded || !selectedDatabase) {
+      return;
+    }
+
+    setDataLoaded(true);
+    const conn = connRef.current;
+
+    // await conn.query(`
+    //   CREATE TABLE house_prices AS
+    //   SELECT * FROM read_parquet('https://www.tablab.app/sample-datasets/house-price.parquet');
+    // `);
+    // const columnData = await conn.query(`SELECT * FROM house_prices`);
+    const columnData = await conn.query(`SELECT * FROM ${selectedDatabase}`);
+    const columsStats = await conn.query(`
     with base_query as (
-      select * from house_prices
+      select * from ${selectedDatabase}
     ),
     cols as (
       unpivot (
@@ -49,34 +84,16 @@ const App: React.FC = () => {
     )
     select value.* from cols
   `);
-  const [customQuery, setCustomQuery] = useState<string>("");
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    if (dataLoaded) {
-      return;
-    }
-    setDataLoaded(true);
-    const db = await instantiate(duckdb);
-    const conn = await db.connect();
-
-    await conn.query(`
-      CREATE TABLE house_prices AS 
-      SELECT * FROM read_parquet('https://www.tablab.app/sample-datasets/house-price.parquet');
-    `);
-    const columnData = await conn.query(`SELECT * FROM house_prices`);
-    const columsStats = await conn.query(customQuery || originalQuery);
 
     const columsCount = await conn.query(`
       with base_query as (
-        select * from house_prices 
+        select * from ${selectedDatabase} 
       )
       SELECT 
       count(*) as count
       FROM base_query
     `);
+    console.log("columsCount", columsCount);
 
     const columns: string[] = columnData.schema.fields.map(
       (field: { name: string }) => field.name
@@ -102,48 +119,45 @@ const App: React.FC = () => {
       };
     });
     setStatsData(combinedData);
-
-    // setStatsData(
-    //   columns.map((column: string) => ({
-    //     label: column,
-    //     type: typeof dataArray[0]?.[column],
-    //     value: dataArray.map((row: Record<string, any>) =>
-    //       typeof row[column] === "bigint"
-    //         ? row[column].toString()
-    //         : row[column]
-    //     ),
-    //   }))
-    // );
-
-    await conn.close();
     setDataLoaded(false);
   };
-  useEffect(() => {
-    if (canvasRef.current && statsData.length > 0) {
-      const ctx = canvasRef.current.getContext("2d");
-      if (ctx) {
-        new Chart(ctx, {
-          type: "bar",
-          data: {
-            labels: statsData.map((d) => d.label),
-            datasets: [
-              {
-                label: "Dữ liệu",
-                data: statsData.map((d) => d.value.length),
-                backgroundColor: "rgba(54, 162, 235, 0.6)",
-              },
-            ],
-          },
-          options: {
-            responsive: true,
-            scales: {
-              y: { beginAtZero: true },
-            },
-          },
-        });
+  const createDatabase = async () => {
+    if (!customQuery.trim()) return;
+
+    const conn = connRef.current;
+
+    try {
+      setCreateLoading(true);
+      // Split each "CREATE TABLE" statement
+      const queryWithSemicolon = customQuery.trim().endsWith(";")
+        ? customQuery
+        : customQuery + ";";
+      const createTableStatements = queryWithSemicolon
+        .split(/;\s*CREATE TABLE/i) // Query by ';' and "CREATE TABLE"
+        .map((stmt, index) => (index === 0 ? stmt : "CREATE TABLE" + stmt)) // Add the "CREATE TABLE" that was lost when splitting.
+        .filter((stmt) => stmt.trim().length > 0); // Filter empty strings
+
+      for (const query of createTableStatements) {
+        await conn.query(query);
+
+        // Query get database name
+        const match = query.match(/CREATE TABLE (\w+)/i);
+        if (match) {
+          const dbName = match[1];
+
+          if (!databases.includes(dbName)) {
+            setDatabases((prev) => [...prev, dbName]);
+          }
+        }
       }
+    } catch (error) {
+      console.error("Query Error:", error);
+    } finally {
+      setCreateLoading(false);
     }
-  }, [statsData]);
+
+    // await conn.close();
+  };
 
   const countUnique = (arr: string[]): number => new Set(arr).size;
   const [itemSelected, setItemSelected] = useState<string | null>(null);
@@ -161,11 +175,29 @@ const App: React.FC = () => {
         placeholder="Enter custom SQL query..."
       />
       <button
-        onClick={fetchData}
+        onClick={createDatabase}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
       >
         Run Query
       </button>
+      {!createLoading ? (
+        databases.length > 0 ? (
+          <select
+            value={selectedDatabase || ""}
+            onChange={(e) => setSelectedDatabase(e.target.value)}
+            className="w-full border p-2 mb-2"
+          >
+            <option value="">Choose Database</option>
+            {databases.map((db, idx) => (
+              <option key={idx} value={db}>
+                {db}
+              </option>
+            ))}
+          </select>
+        ) : null
+      ) : (
+        <p>Creating database...</p>
+      )}
       {!dataLoaded ? (
         statsData.map((stats, idx) => (
           <div className="m-1">
